7f06321c063805661687e846110e19c8feb8c103
LelisCis <leliuscris@gmail.com>
2022-05-24 02:13:40 -0300

    act

---
+++ js/appi.js
@@ -0,0 +1,14 @@
+(function() {
+    const ishow = 115;
+    const $divtop = document.getElementById("div-totop")
+    window.addEventListener("scroll", function() {
+        if(document.documentElement.scrollTop > ishow){
+            $divtop.style.display = "inherit"
+        }
+        else {
+            $divtop.style.display = "none"
+        }
+    })
+})()
+
+
